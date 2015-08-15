67462c553fe0a91a5a336b55195527f612c2069c
Jens Geudens <jensgeudens@hotmail.com>
2015-08-15 23:49:54 +0200

    Use axis scale options to decide to rescale (watch file)

--- src/extendedgraphview.cpp
+++ src/extendedgraphview.cpp
@@ -22,24 +22,7 @@ void ExtendedGraphView::addGraphs(QList<QList<double> > data)
 
 void ExtendedGraphView::updateData(QList<QList<double> > * pDataLists)
 {
-    bool bFullScale = false;
     const QVector<double> timeData = pDataLists->at(0).toVector();
-
-    if (_pPlot->graph(0)->data()->keys().size() > 0)
-    {
-        if (
-        (_pPlot->xAxis->range().lower <= _pPlot->graph(0)->data()->keys().first())
-        && (_pPlot->xAxis->range().upper >= _pPlot->graph(0)->data()->keys().last())
-        )
-        {
-            bFullScale = true;
-        }
-    }
-    else
-    {
-        /* First load of file: always rescale */
-        bFullScale = true;
-    }
 
     for (qint32 i = 1; i < pDataLists->size(); i++)
     {
@@ -48,10 +31,23 @@ void ExtendedGraphView::updateData(QList<QList<double> > * pDataLists)
         _pPlot->graph(i - 1)->setData(timeData, graphData);
     }
 
-    if (bFullScale)
+    // Rescale only axis that are in auto scale mode
+    if (
+        (_pGuiModel->xAxisScalingMode() == SCALE_AUTO)
+        && (_pGuiModel->xAxisScalingMode() == SCALE_AUTO)
+    )
     {
         _pPlot->rescaleAxes(true);
     }
+    else if (_pGuiModel->xAxisScalingMode() == SCALE_AUTO)
+    {
+        _pPlot->xAxis->rescale(true);
+    }
+    else if (_pGuiModel->yAxisScalingMode() == SCALE_AUTO)
+    {
+       _pPlot->yAxis->rescale(true);
+    }
+
     _pPlot->replot();
 }
 
